4f567f23e86db3355cd2e9cbc7fc89428a93e345
Artur Haddad <artur@artur.me>
2018-10-24 15:38:24 -0300

    fill 'currently reading' shelf with respective books

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ class BooksApp extends React.Component {
   }
 
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -60,68 +62,45 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div
-                              className="book-cover"
-                              style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage:
-                                  'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                              }}
-                            />
-                            <div className="book-shelf-changer">
-                              <select onChange={this.getBooks}>
-                                <option value="move" disabled>
-                                  Move to...
-                                </option>
-                                <option value="currentlyReading">
-                                  Currently Reading
-                                </option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
+                      {books
+                        .filter(book => book.shelf === "currentlyReading")
+                        .map(book => (
+                          <li key={book.id}>
+                            <div className="book">
+                              <div className="book-top">
+                                <div
+                                  className="book-cover"
+                                  style={{
+                                    width: 128,
+                                    height: 188,
+                                    backgroundImage: `url(${
+                                      book.imageLinks.smallThumbnail
+                                    })`
+                                  }}
+                                />
+                                <div className="book-shelf-changer">
+                                  <select>
+                                    <option value="move" disabled>
+                                      Move to...
+                                    </option>
+                                    <option value="currentlyReading">
+                                      Currently Reading
+                                    </option>
+                                    <option value="wantToRead">
+                                      Want to Read
+                                    </option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}</div>
+                              <div className="book-authors">
+                                {book.authors.join(", ")}
+                              </div>
                             </div>
-                          </div>
-                          <div className="book-title">
-                            To Kill a Mockingbird
-                          </div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div
-                              className="book-cover"
-                              style={{
-                                width: 128,
-                                height: 188,
-                                backgroundImage:
-                                  'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
-                              }}
-                            />
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>
-                                  Move to...
-                                </option>
-                                <option value="currentlyReading">
-                                  Currently Reading
-                                </option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
+                          </li>
+                        ))}
                     </ol>
                   </div>
                 </div>
