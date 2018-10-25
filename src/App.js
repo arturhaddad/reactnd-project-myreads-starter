1b2d6248a50baa413cf546439ae3908363658d15
Artur Haddad <artur@artur.me>
2018-10-25 16:12:58 -0300

    install react-router-dom and route pages

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 
-import BookList from "./components/BookList";
-import Header from "./components/Header";
+import Main from "./pages/Main";
+import Search from "./pages/Search";
 
-class BooksApp extends React.Component {
-  state = {};
-
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <Header />
-            <BookList />
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </a>
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const BooksApp = () => (
+  <div className="app">
+    <Route exact path="/" component={Main} />
+    <Route exact path="/search" component={Search} />
+  </div>
+);
 
 export default BooksApp;
