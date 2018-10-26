7180498df4cf505fabe5d64522b198d2ea3817a7
Artur Haddad <artur@artur.me>
2018-10-26 13:51:10 -0300

    change book cover initial position-y to top

--- src/components/BookItem/styles.js
+++ src/components/BookItem/styles.js
@@ -4,11 +4,12 @@ export const Book = styled.li`
   .book-cover {
     width: 128px;
     height: 188px;
-    background: no-repeat center;
+    background: no-repeat top center;
     background-size: cover;
   }
 
   .book-cover.empty-thumbnail-true {
     background-size: inherit;
+    background-position: center;
   }
 `;
